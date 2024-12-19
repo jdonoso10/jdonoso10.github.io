1c39288c70389b324751b53604c06cdea0bd5c88
jdonoso10 <jesus.donoso.diez@gmail.com>
2024-12-19 20:12:29 +0100

    cosas en casa1

--- app.js
+++ app.js
@@ -1,111 +1,109 @@
-const URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=51ec4268';
+const API_KEY = "51ec4268";
+const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
 
 let peliculas;
 let peliculasFiltradas;
 
 function processMovie(data) {
-    peliculas = data.Search;
-    peliculasFiltradas = Array.from(peliculas);//crea nuevo Array
-    generarDesplegableGenero(peliculas);
+  peliculas = data.Search;
+  peliculasFiltradas = Array.from(peliculas); //crea nuevo Array
+  generarDesplegableGenero(peliculas);
 
-    //     Recorremos con el método forEach:
-    peliculas.forEach(pelicula => {
-        generateCard(pelicula);
-        // console.log("Título: " + pelicula.Title);
-
-    });
+  //     Recorremos con el método forEach:
+  peliculas.forEach((pelicula) => {
+    generateCard(pelicula);
+    // console.log("Título: " + pelicula.Title);
+  });
 }
 
 function generateCard(pelicula) {
-    //0. mostrar nº de resultados
-    document.querySelector("#resultados").textContent="Mostrando: " + peliculasFiltradas.length + " resultado(s)";
-    console.log(peliculasFiltradas.length);
+  //0. mostrar nº de resultados
+  document.querySelector("#resultados").textContent =
+    "Mostrando: " + peliculasFiltradas.length + " resultado(s)";
+  console.log(peliculasFiltradas.length);
 
-    //1. Crear la tarjeta
-    const nuevaCard = document.createElement("div");//Crea un elemento de tipo div
-    nuevaCard.setAttribute("class", "card");
+  //1. Crear la tarjeta
+  const nuevaCard = document.createElement("div"); //Crea un elemento de tipo div
+  nuevaCard.setAttribute("class", "card");
 
-    //2. Crear la imagen
-    const nuevaImg = document.createElement("img");
-    nuevaImg.setAttribute("src", pelicula.Poster);
-    nuevaImg.setAttribute("alt", `Póster de la película ${pelicula.Title}`);
-    nuevaCard.appendChild(nuevaImg);
+  //2. Crear la imagen
+  const nuevaImg = document.createElement("img");
+  nuevaImg.setAttribute("src", pelicula.Poster);
+  nuevaImg.setAttribute("alt", `Póster de la película ${pelicula.Title}`);
+  nuevaCard.appendChild(nuevaImg);
 
-    //3. Crear el contenido de la tarjeta
-    const nuevoContenido = document.createElement("div");
-    nuevoContenido.setAttribute("class", "card-content");
-    nuevaCard.appendChild(nuevoContenido);
+  //3. Crear el contenido de la tarjeta
+  const nuevoContenido = document.createElement("div");
+  nuevoContenido.setAttribute("class", "card-content");
+  nuevaCard.appendChild(nuevoContenido);
 
-    //4. Crear el h3 del título <h3>El Padrino</h3>
-    const nuevoTitulo = document.createElement("h3");
-    nuevoTitulo.textContent = pelicula.Title;
-    nuevoContenido.appendChild(nuevoTitulo);
+  //4. Crear el h3 del título <h3>El Padrino</h3>
+  const nuevoTitulo = document.createElement("h3");
+  nuevoTitulo.textContent = pelicula.Title;
+  nuevoContenido.appendChild(nuevoTitulo);
 
-    //5. Crear el director <p><strong>Director:</strong> Francis Ford Coppola</p>
-    const nuevoParrafoDirector = document.createElement("p");
-    const nuevaNegrita = document.createElement("strong");
-    nuevoParrafoDirector.appendChild(nuevaNegrita);
-    nuevaNegrita.textContent = "Director: ";
-    nuevoContenido.appendChild(nuevoParrafoDirector);
-    const textoDirector = document.createTextNode(pelicula.Director);
-    nuevoParrafoDirector.appendChild(textoDirector);
+  //5. Crear el director <p><strong>Director:</strong> Francis Ford Coppola</p>
+  const nuevoParrafoDirector = document.createElement("p");
+  const nuevaNegrita = document.createElement("strong");
+  nuevoParrafoDirector.appendChild(nuevaNegrita);
+  nuevaNegrita.textContent = "Director: ";
+  nuevoContenido.appendChild(nuevoParrafoDirector);
+  const textoDirector = document.createTextNode(pelicula.Director);
+  nuevoParrafoDirector.appendChild(textoDirector);
 
-    //6. Año
-    const nuevoParrafoAno = document.createElement("p");
-    const nuevaNegrilla = document.createElement("strong");
-    nuevoParrafoAno.appendChild(nuevaNegrilla);
-    nuevaNegrilla.textContent = "Año: ";
-    nuevoContenido.appendChild(nuevoParrafoAno);
-    const textAno = document.createTextNode(pelicula.Year);
-    nuevoParrafoAno.appendChild(textAno);
+  //6. Año
+  const nuevoParrafoAno = document.createElement("p");
+  const nuevaNegrilla = document.createElement("strong");
+  nuevoParrafoAno.appendChild(nuevaNegrilla);
+  nuevaNegrilla.textContent = "Año: ";
+  nuevoContenido.appendChild(nuevoParrafoAno);
+  const textAno = document.createTextNode(pelicula.Year);
+  nuevoParrafoAno.appendChild(textAno);
 
-    //7. Género
-    const nuevoParrafoGenero = document.createElement("p");
-    const nuevoSpanGenero = document.createElement("strong"); // Crear un nuevo span para el género
-    nuevoParrafoGenero.appendChild(nuevoSpanGenero);
-    nuevoSpanGenero.textContent = "Género: ";
-    nuevoContenido.appendChild(nuevoParrafoGenero);
-    const textGenero = document.createTextNode(pelicula.Genre);
-    nuevoParrafoGenero.appendChild(textGenero);
+  //7. Género
+  const nuevoParrafoGenero = document.createElement("p");
+  const nuevoSpanGenero = document.createElement("strong"); // Crear un nuevo span para el género
+  nuevoParrafoGenero.appendChild(nuevoSpanGenero);
+  nuevoSpanGenero.textContent = "Género: ";
+  nuevoContenido.appendChild(nuevoParrafoGenero);
+  const textGenero = document.createTextNode(pelicula.Genre);
+  nuevoParrafoGenero.appendChild(textGenero);
 
-    //8. Duración
-    const nuevoParrafoDuracion = document.createElement("p");
-    const nuevoSpanDuracion = document.createElement("strong"); // Crear un nuevo span para la duración
-    nuevoParrafoDuracion.appendChild(nuevoSpanDuracion);
-    nuevoSpanDuracion.textContent = "Duración: ";
-    nuevoContenido.appendChild(nuevoParrafoDuracion);
-    const textDuracion = document.createTextNode(pelicula.Runtime);
-    nuevoParrafoDuracion.appendChild(textDuracion);
+  //8. Duración
+  const nuevoParrafoDuracion = document.createElement("p");
+  const nuevoSpanDuracion = document.createElement("strong"); // Crear un nuevo span para la duración
+  nuevoParrafoDuracion.appendChild(nuevoSpanDuracion);
+  nuevoSpanDuracion.textContent = "Duración: ";
+  nuevoContenido.appendChild(nuevoParrafoDuracion);
+  const textDuracion = document.createTextNode(pelicula.Runtime);
+  nuevoParrafoDuracion.appendChild(textDuracion);
 
-
-    //Último paso: Agregar al contenedor la ficha recién creada
-    document.querySelector("#container").appendChild(nuevaCard);//Agregamos el div al contenedor
+  //Último paso: Agregar al contenedor la ficha recién creada
+  document.querySelector("#container").appendChild(nuevaCard); //Agregamos el div al contenedor
 }
 
 //***** Funcion para sacar los géneros del .json y crear campos en el desplegable de html
 function generarDesplegableGenero(peliculas) {
-    //Extraemos los géneros del fichero json 
-    let setGeneros = new Set();
-    peliculas.forEach(pelicula => {
-        let generos = pelicula.Genre.split(',').map(genero => genero.trim());
-        generos.forEach(genero => setGeneros.add(genero));
+  //Extraemos los géneros del fichero json
+  let setGeneros = new Set();
+  peliculas.forEach((pelicula) => {
+    let generos = pelicula.Genre.split(",").map((genero) => genero.trim());
+    generos.forEach((genero) => setGeneros.add(genero));
+  });
 
-    });
-
-    //<option value="drama">Drama</option>
-    let arrayGeneros = Array.from(setGeneros);
-    arrayGeneros.sort().forEach(genero => {
-        let generoOption = document.createElement("option");
-        generoOption.setAttribute("value", genero.toLowerCase());
-        generoOption.textContent = genero;
-        document.querySelector("#s-genero").appendChild(generoOption);
-    });
+  //<option value="drama">Drama</option>
+  let arrayGeneros = Array.from(setGeneros);
+  arrayGeneros.sort().forEach((genero) => {
+    let generoOption = document.createElement("option");
+    generoOption.setAttribute("value", genero.toLowerCase());
+    generoOption.textContent = genero;
+    document.querySelector("#s-genero").appendChild(generoOption);
+  });
 }
 
 function clearCards() {
-    //document.querySelector("#container").innerHTML="";//Chapuza
-    document.querySelectorAll(".card").forEach(card => card.remove());//Elegante
+  //document.querySelector("#container").innerHTML="";//Chapuza
+  document.querySelectorAll(".card").forEach((card) => card.remove()); //Elegante
 }
-
 
 doGetRequest(URL, processMovie);
--- gestor-busqueda.js
+++ gestor-busqueda.js
@@ -1,124 +1,23 @@
-
 //Se busca pulsando el boton buscar de tiulo
 document.querySelector("#b-titulo").addEventListener("click", () => {
-    filtrarPeliculas("#t-titulo", "Title");
+  filtrarPeliculas("#t-titulo", "Title");
 });
-
-//Se busca por cada pulsación en la caja de texto de Título
-/*
-document.querySelector("#t-titulo").addEventListener("input", () => {
-    filtrarPeliculas("#t-titulo","Title");
-});
-
-
-document.querySelector("#b-actor").addEventListener("click", () => {
-    filtrarPeliculas("#t-actor", "Actors");
-});
-
-//Se busca cuando se pulsa el botón de Buscar en la sección género
-/*
-document.querySelector("#b-genero").addEventListener("click", () => {
-    filtrarPeliculas("#s-genero","Genre");
-});
-*/
 
 document.querySelector("#s-genero").addEventListener("change", () => {
-    filtrarPeliculas("#s-genero", "Genre");
+  filtrarPeliculas("#s-genero", "Genre");
 });
 
 document.querySelector("#b-anyo").addEventListener("click", () => {
-    filtrarPeliculas("#t-anyo", "Year");
+  filtrarPeliculas("#t-anyo", "Year");
 });
 
 function filtrarPeliculas(idElementoBusqueda, nombreAtributoBusqueda) {
-    clearCards();
-    const textoBusqueda = document.querySelector(idElementoBusqueda).value;
-    peliculasFiltradas =
-        peliculas.filter(pelicula =>
-            pelicula[nombreAtributoBusqueda]
-                .toUpperCase()
-                .includes(textoBusqueda.trim().toUpperCase()));
-    peliculasFiltradas.map(generateCard);
+  clearCards();
+  const textoBusqueda = document.querySelector(idElementoBusqueda).value;
+  peliculasFiltradas = peliculas.filter((pelicula) =>
+    pelicula[nombreAtributoBusqueda]
+      .toUpperCase()
+      .includes(textoBusqueda.trim().toUpperCase())
+  );
+  peliculasFiltradas.map(generateCard);
 }
-
-
-
-// ********** Primera version antes de refactorizar el código ********
-
-
-// //busca por título
-// document.querySelector("#b-titulo").addEventListener("click", () => {
-//     clearCards();
-//     const textoBusqueda = document.querySelector("#t-titulo").value;
-//     const peliculasFiltradas =
-//         peliculas.filter(pelicula =>
-//             pelicula.Title
-//                 .toUpperCase()
-//                 .includes(textoBusqueda.trim().toUpperCase()));
-
-
-//     peliculasFiltradas.forEach(pelicula => {
-//         generateCard(pelicula);
-//     });
-//     // otra forma de filtrar, en lugar de "forEach", usando "map" pero funciona igual:
-//     // peliculasFiltradas.map(generateCard);
-// });
-
-// /*
-// //Se busca por cada pulsación
-// document.querySelector("#t-titulo").addEventListener("input", () => {
-//     clearCards();
-//     const textoBusqueda = document.querySelector("#t-titulo").value;
-//     const peliculasFiltradas =
-//         peliculas.filter(pelicula =>
-//             pelicula.Title
-//                 .toUpperCase()
-//                 .includes(textoBusqueda.trim().toUpperCase()));
-//     peliculasFiltradas.forEach(pelicula => {
-//         generateCard(pelicula);
-//     });
-// });
-// */
-
-
-// busca por nombre de actor/actriz
-// document.querySelector("#b-actor").addEventListener("click", () => {
-//     clearCards();
-//     const textoBusqueda = document.querySelector("#t-actor").value;
-//     const peliculasFiltradas =
-//         peliculas.filter(pelicula =>
-//             pelicula.Actors
-//                 .toUpperCase()
-//                 .includes(textoBusqueda.trim().toUpperCase()));
-//     peliculasFiltradas.forEach(pelicula => {
-//         generateCard(pelicula);
-//     });
-// });
-
-// busca por Género en el desplegable
-// document.querySelector("#s-genero").addEventListener("change", () => {
-//     clearCards();
-//     const textoBusqueda = document.querySelector("#s-genero").value;
-//     const peliculasFiltradas =
-//         peliculas.filter(pelicula =>
-//             pelicula.Genre
-//                 .toUpperCase()
-//                 .includes(textoBusqueda.trim().toUpperCase()));
-//     peliculasFiltradas.forEach(pelicula => {
-//         generateCard(pelicula);
-//     });
-// });
-
-// busca por año
-// document.querySelector("#b-anyo").addEventListener("click", () => {
-//     clearCards();
-//     const textoBusqueda = document.querySelector("#t-anyo").value;
-//     const peliculasFiltradas =
-//         peliculas.filter(pelicula =>
-//             pelicula.Year
-//                 .toUpperCase()
-//                 .includes(textoBusqueda.trim().toUpperCase()));
-//     peliculasFiltradas.forEach(pelicula => {
-//         generateCard(pelicula);
-//     });
-// });
